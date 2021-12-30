66bdbde18bae96be8e7c701c7ff030e0a04d7463
amauri dias <athlon.amauri@gmail.com>
2021-12-30 09:31:12 -0300

    react query study

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {QueryClientProvider, QueryClient} from 'react-query';
+
+
+const client = new QueryClient({});
+
+ReactDOM.render(
+    <React.StrictMode>
+        <QueryClientProvider client={client}>
+            <App/>
+        </QueryClientProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
